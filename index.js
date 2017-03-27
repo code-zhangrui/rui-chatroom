c83c124ab40f9098766da77b18fc99d8c544c5e4
code-zhangrui <code.zhangrui@gmail.com>
2017-03-27 23:36:13 +0800

    基本聊天功能有了

--- index.js
+++ index.js
@@ -16,15 +16,24 @@ server.listen(3000);
 //所以代码io.sockets.emit('foo')表示所有人都可以收到该事件
 io.on('connection',function(socket){
     socket.on('login',function(nickname){ //设置昵称
-        if(usersindexOf(nickname)>-1){
+        if(users.indexOf(nickname)>-1){
             socket.emit('nickExisted');
         }else{
             socket.userIndex=users.length; //因为是数组最后一个元素，所以索引就是数组的长度users.length
             socket.nickname=nickname;
             users.push(nickname);
             socket.emit('loginSuccess');
-            io.sockets.emit('system',nickname);//向所有连接到服务器的客户端发送当前登陆用户的昵称 
+            io.sockets.emit('system',nickname,users.length,'login');//向所有连接到服务器的客户端发送当前登陆用户的昵称 
         };
+    });
+
+    socket.on('disconnect',function(){//断开事件
+        users.splice(socket.userIndex,1);
+        socket.broadcast.emit('system',socket.nickname,users.length,'logout');
+    });
+
+    socket.on('postMsg',function(msg){//接收发送聊天
+        socket.broadcast.emit('newMsg',socket.nickname,msg);
     });
 });
 
--- src/chat.js
+++ src/chat.js
@@ -33,8 +33,40 @@ Chatroom.prototype={
 
         this.socket.on('loginSuccess',function(){
             document.title="聊天室|"+document.getElementById('nicknameInput').value;
-             document.getElementById('loginWrapper').style.display = 'none';//隐藏遮罩层显聊天界面
-     document.getElementById('messageInput').focus();//让消息输入框获得焦点
+            document.getElementById('loginWrapper').style.display = 'none';//隐藏遮罩层显聊天界面
+            document.getElementById('messageInput').focus();//让消息输入框获得焦点
         });
+
+        this.socket.on('system',function(nickName, userCount, type){//判断system信息的类别
+            var msg=nickName+(type=='login'?'加入':'离开');
+            _this._displayNewMsg('system',msg,'red');
+            document.getElementById('status').textCount=userCount+(userCount>1?'users':'user')+'online';
+        });
+
+        this.socket.on('newMsg',function(user,msg){
+            _this._displayNewMsg(user,msg);
+        })
+
+        document.getElementById('sendBtn').addEventListener('click',function(){//发送聊天
+            var messageInput=document.getElementById('messageInput'),
+                msg=messageInput.value;
+            messageInput.value='';
+            messageInput.focus();
+            if(msg.trim().length!=0){
+                _this.socket.emit('postMsg',msg);//给服务器的
+                _this._displayNewMsg('me',msg);//给自己的
+            };
+        },false);
+    },
+
+    _displayNewMsg:function(user,msg,color){ //显示信息函数
+        var container=document.getElementById('historyMsg'),
+            msgToDisplay=document.createElement('p'),
+            data=new Date().toTimeString().substr(0,8);
+
+        msgToDisplay.style.color=color||'#000';
+        msgToDisplay.innerHTML=user+'<span class="timespan">('+data+'):</span>'+msg;
+        container.appendChild(msgToDisplay);
+        container.scrollTop=container.scrollHeight;
     }
-}
+};
